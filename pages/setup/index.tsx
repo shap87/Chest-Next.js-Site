7d7a3c6e379b063115baf867c5d64df1eed85ee5
shap87 <shane87web@gmail.com>
2022-11-18 18:10:08 +0200

    fix: remove all pages

--- pages/setup/index.tsx
+++
@@ -1,8 +0,0 @@
-import { useRouter } from "next/router";
-import { useEffect } from "react";
-
-export default function Setup() {
-  const router = useRouter();
-  useEffect(() => {router.push("/setup/name")}, []);
-  return <></>;
-}
