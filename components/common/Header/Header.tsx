e78bc1b99d5d949dfa8b28bd545443a204636649
shap87 <shane87web@gmail.com>
2022-11-22 02:32:32 +0200

    features: update header for logged

--- components/common/Header/Header.tsx
+++ components/common/Header/Header.tsx
@@ -1,29 +1,51 @@
 // libs
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import cn from "classnames";
 
 // components
 import { Button } from "../Button/Button";
 import { routes } from "../../../utils/routes";
 
+// assets
+import styles from "./Header.module.scss";
+
 export const Header = () => {
-  const [showLogin, setShowLogin] = useState<boolean>(true);
+  const [isLogin, setIsLogin] = useState<boolean>(true);
+  const [isLogged, setIsLogged] = useState<boolean>(false);
+
   const router = useRouter();
 
   useEffect(() => {
-    if (router.asPath.includes(routes.login)) setShowLogin(false);
+    if (router.asPath.includes(routes.login)) setIsLogin(false);
+    if (router.asPath.includes(routes.profile)) setIsLogged(true);
   }, [])
 
   return (
-    <header className='py-8 md:py-16'>
+    <header className={cn('bg-white py-8 md:py-16', { [styles.logged]: isLogged })}>
       <div className='container'>
         <div className='flex items-center justify-between'>
-          <a href={routes.home} className='w-[120px] md:w-[196px]'>
+          <a href={routes.home} className={cn('w-[126px] md:w-[196px]', styles.logo)}>
             <img src={'./logo.svg'} alt='' />
           </a>
-          {showLogin
-            ? <Button href={routes.login} classname='!px-5 !py-3'>Login</Button>
-            : <Button href='#' classname='!px-5 !py-3'>Sign Up</Button>}
+          {isLogged
+            ? <ul className={cn('flex items-center gap-x-6 md:gap-x-12', styles.nav)}>
+              <li>
+                <img src={'./ball.svg'} alt='' />
+              </li>
+              <li className='relative'>
+                <div
+                  className='absolute z-10 -top-1 -right-1 text-[11px] text-white flex items-center justify-center font-bold bg-primary h-4 p-0.5 min-w-[16px] rounded-full border border-white pointer-events-none'>
+                  2
+                </div>
+                <img src={'./bell.svg'} alt='' />
+              </li>
+              <li>
+                <img src={'./chest.svg'} alt='' />
+              </li>
+            </ul>
+            : isLogin ? <Button href={routes.login} classname='!px-5 !py-3'>Login</Button>
+              : <Button href='#' classname='!px-5 !py-3'>Sign Up</Button>}
         </div>
       </div>
     </header>
