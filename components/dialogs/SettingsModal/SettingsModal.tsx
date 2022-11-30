b85ad707dc037c1a57d2d46ad133ac1a1b7010e0
shap87 <shane87web@gmail.com>
2022-11-30 03:34:13 +0200

    feature: add "share" save to clipboard

--- components/dialogs/SettingsModal/SettingsModal.tsx
+++ components/dialogs/SettingsModal/SettingsModal.tsx
@@ -1,15 +1,23 @@
-// libs
 import { FC } from "react";
+
+// libs
+import { useRouter } from "next/router";
 
 // components
 import { Button, ModalBaseLayout } from "../../common";
+import { logout, useFirebase } from "../../../context/firebase";
+import { routes } from "../../../utils/routes";
 
 interface EditProfileProps {
   show: boolean;
   onClose: () => void;
+  setLoading: (value: boolean) => void;
 }
 
-export const SettingsModal: FC<EditProfileProps> = ({ show, onClose }) => {
+export const SettingsModal: FC<EditProfileProps> = ({ show, onClose, setLoading }) => {
+  const router = useRouter();
+
+  const firebaseApp = useFirebase();
 
   return (
     <ModalBaseLayout
@@ -25,7 +33,16 @@ export const SettingsModal: FC<EditProfileProps> = ({ show, onClose }) => {
         </div>
       }>
       <div className="w-full flex flex-col items-center py-10 md:py-24 max-w-[250px] mx-auto">
-        <Button classname="w-full mb-10 group" icon="icon-right">
+        <Button
+          classname="w-full mb-10 group"
+          icon="icon-right"
+          onClick={() => {
+            setLoading(true);
+            logout(firebaseApp).then(() => {
+              setLoading(false);
+              router.push(routes.home)
+            });
+          }}>
           Sign out
           <img className="group-hover:invert" src={'./sign-out.svg'} alt='' />
         </Button>
--- components/layout/profile/User/User.tsx
+++ components/layout/profile/User/User.tsx
@@ -3,18 +3,21 @@ import { useState } from "react";
 import cn from "classnames";
 
 // components
-import { H5, H6 } from "../../../common";
+import { H5, H6, LoadingSpinner } from "../../../common";
 import { EditProfileModal, SettingsModal } from "../../../dialogs";
 
 // assets
 import styles from "../../../../styles/profile.module.scss";
 
 export const User = () => {
+  const [loading, setLoading] = useState<boolean>(false);
   const [showEditProfileModal, setShowEditProfileModal] = useState<boolean>(false);
   const [showSettingsModal, setShowSettingsModal] = useState<boolean>(false);
+  const [share, setShare] = useState<boolean>(false);
 
   return (
     <>
+      {loading && <LoadingSpinner />}
       <section className="py-4 md:py-8">
         <div className="container">
           <div className="flex justify-between items-center">
@@ -40,7 +43,13 @@ export const User = () => {
                     Edit profile
                     <img src={"./edit.svg"} alt="" />
                   </li>
-                  <li>
+                  <li onClick={() => {
+                    navigator.clipboard.writeText(`${window.location.origin}#id`);
+                    setShare(true);
+                    setTimeout(() => {
+                      setShare(false)
+                    }, 3000)
+                  }}>
                     Share
                     <img src={"./share.svg"} alt="" />
                   </li>
@@ -72,12 +81,18 @@ export const User = () => {
       </section>
       <EditProfileModal
         show={showEditProfileModal}
-        onClose={() => setShowEditProfileModal(false)}
-      />
+        onClose={() => setShowEditProfileModal(false)} />
       <SettingsModal
         show={showSettingsModal}
         onClose={() => setShowSettingsModal(false)}
-      />
+        setLoading={setLoading} />
+      {share && <div className="fixed z-50 bottom-8 md:top-24 left-1/2 -translate-x-1/2">
+        <div
+          className="bg-[#FFF4FA] py-2 px-5 flex justify-center items-center border border-[#FF9AD4] rounded-[10px]">
+          <img className="w-5 mr-4" src={"./share-link.svg"} alt="" />
+          <H6 classname="text-[#FF0098] !mb-0 whitespace-nowrap">Profile link copied</H6>
+        </div>
+      </div>}
     </>
   );
 };
